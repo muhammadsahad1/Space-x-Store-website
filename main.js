9eae06af1816cd337f479eb1967b6e66fb21046e
muhammad sahad <147528419+muhammadsahad1@users.noreply.github.com>
2023-10-19 21:06:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+let togglelogo = document.querySelector('.toggle-logo')
+let menu = document.querySelector('.menu')
+
+togglelogo.addEventListener('click',function(){
+
+  menu.classList.toggle('is-active')
+})
